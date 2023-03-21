fa581520d67a32c941d742e95149b36d5d10ee33
259ayano <a.hirayama@newdaysys.co.jp>
2023-03-21 13:39:28 +0900

    Update web-qa.py

--- apps/web-crawl-q-and-a/web-qa.py
+++ apps/web-crawl-q-and-a/web-qa.py
@@ -13,8 +13,6 @@ import os
 import pandas as pd
 import tiktoken
 import openai
-from openai.embeddings_utils import distances_from_embeddings
-import pandas as pd
 import numpy as np
 from openai.embeddings_utils import distances_from_embeddings, cosine_similarity
 
@@ -249,11 +247,11 @@ def split_into_many(text, max_tokens = max_tokens):
         # Otherwise, add the sentence to the chunk and add the number of tokens to the total
         chunk.append(sentence)
         tokens_so_far += token + 1
-
+        
     # Add the last chunk to the list of chunks
     if chunk:
         chunks.append(". ".join(chunk) + ".")
-        
+
     return chunks
     
 
@@ -285,6 +283,9 @@ df.n_tokens.hist()
 ################################################################################
 ### Step 10
 ################################################################################
+
+# Note that you may run into rate limit issues depending on how many files you try to embed
+# Please check out our rate limit guide to learn more on how to handle this: https://platform.openai.com/docs/guides/rate-limits
 
 df['embeddings'] = df.text.apply(lambda x: openai.Embedding.create(input=x, engine='text-embedding-ada-002')['data'][0]['embedding'])
 df.to_csv('processed/embeddings.csv')
